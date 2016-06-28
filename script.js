858587ef3915ab3ce16df8085a7b020b016bd156
Kolasani <DEEPTHI.KOLASANI@tetratech.com>
2016-06-28 13:33:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var app = angular.module('app',[]);
+app.controller('AppCtrl', function($http){
+var vm= this;
+
+
+
+vm.submit = function () {
+    var params = {
+    method: 'flickr.photos.search',
+    api_key: '932e0cc959dd923ba91dafb4a7a720ef',
+    tags: vm.search_tag,
+    format: 'json',
+    nojsoncallback: 1
+}
+    vm.startSearching =true;
+           $http.get('https://api.flickr.com/services/rest',{params : params})
+            .then(function(res){
+                vm.data = res.data.photos.photo;
+                })
+                .finally(function (){
+                vm.startSearching =false;  
+                });
+}
+});
